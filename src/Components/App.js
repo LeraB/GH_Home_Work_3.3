fb35a115a108a0fd8f723503349ffb4217c9a09b
Валерійка <kudravka13@gmail.com>
2017-11-03 17:56:54 +0200

    moved files

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import {CarouselProvider, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import logo from './img/logo.svg';
-import './App.css';
-import Mycomponent from './Mycomponent'
-
-
+import logo from '../img/logo.svg';
+import '../Style/App.css';
 
 
 export default class extends React.Component {
@@ -19,9 +16,9 @@ export default class extends React.Component {
                 totalSlides={3}
             >
                 <Slider>
-                    <Slide index={0}><img src={require('./img/malenkij-kotik-s-vnimatelnym-vzglyadom.jpg')}/></Slide>
-                    <Slide index={1}><img src={require('./img/maxresdefault.jpg')}/></Slide>
-                    <Slide index={2}><img src={require('./img/maxresdefault (1).jpg')}/></Slide>
+                    <Slide index={0}><img src={require('../img/malenkij-kotik-s-vnimatelnym-vzglyadom.jpg')}/></Slide>
+                    <Slide index={1}><img src={require('../img/maxresdefault.jpg')}/></Slide>
+                    <Slide index={2}><img src={require('../img/maxresdefault (1).jpg')}/></Slide>
                 </Slider>
                 <ButtonBack>Back</ButtonBack>
                 <ButtonNext>Next</ButtonNext>
